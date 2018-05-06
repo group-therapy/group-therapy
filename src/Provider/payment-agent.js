5160d9f432a0039841c1fbb8e65d6746dba46a2a
Zachary Lim <zachary.lim@hotmail.com>
2018-05-06 10:52:36 -0700

    fix token

--- src/Provider/payment-agent.js
+++ src/Provider/payment-agent.js
@@ -9,14 +9,7 @@ const PAYMENT_AMOUNT = 10;
 
 const responseBody = res => res.body;
 
-let token = superagent.post('https://hack.softheon.io/oauth2/connect/token').type('form')
-.send('client_id=1b9f1295-4c09-4d50-b8d6-b1d5ee68b702')
-.send('client_secret=769f06a8-b185-4791-9afc-cf9a5caceb4d')
-.send('grant_type=client_credentials')
-.send('scope=paymentapi')
-.send('username=hack059')
-.send('password=2jYQNxwY').then(res => responseBody(res).access_token);
-console.log(token);
+let token = null;
 const tokenPlugin = req => {
   token = window.localStorage.getItem('jwt');
   if (window.localStorage.getItem('jwt')) {
