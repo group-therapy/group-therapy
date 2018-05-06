4fd338e06c5e086e875093dab40f55f69f99b226
Edmund Mok <edmundmok@outlook.com>
2018-05-06 08:42:04 -0700

    Header for patient and phys

--- src/Container/Patient.js
+++ src/Container/Patient.js
@@ -3,22 +3,34 @@ import { OTSession, OTStreams } from 'opentok-react';
 import ConnectionStatus from '../Component/ConnectionStatus';
 import Publisher from '../Component/Publisher';
 import Subscriber from '../Component/Subscriber';
+import { Grid, Header } from 'semantic-ui-react';
+
 
 class Patient extends Component {
 	render() {
 		const {apiKey, sessionId, token, sessionEvents} = this.props;
 		return (
 		<div>
-			<OTSession
-	          apiKey={this.props.apiKey}
-	          sessionId={this.props.sessionId}
-	          token={this.props.token}
-	          eventHandlers={this.sessionEvents}
-	          onError={this.onError}>
-	          {this.props.error ? <div>{this.props.error}</div> : null}
-	          <ConnectionStatus connected={this.props.connected} />
-	          <Publisher />
-	        </OTSession>
+			<Grid>
+				<Grid.Row>
+					<Grid.Column>
+						<Header as='h1'>Remotely Care</Header>
+					</Grid.Column>
+				</Grid.Row>
+				<OTSession
+          apiKey={this.props.apiKey}
+          sessionId={this.props.sessionId}
+          token={this.props.token}
+          eventHandlers={this.sessionEvents}
+          onError={this.onError}>
+          {this.props.error ? <div>{this.props.error}</div> : null}
+          <ConnectionStatus connected={this.props.connected} />
+          <Publisher />
+          <OTStreams>
+	         <Subscriber />
+          </OTStreams>
+        </OTSession>
+	    </Grid>
 		</div>);
 	}
 }
--- src/Container/Physician.js
+++ src/Container/Physician.js
@@ -3,25 +3,33 @@ import { OTSession, OTStreams } from 'opentok-react';
 import ConnectionStatus from '../Component/ConnectionStatus';
 import Publisher from '../Component/Publisher';
 import Subscriber from '../Component/Subscriber';
+import { Grid, Header } from 'semantic-ui-react';
 
 class Physician extends Component {
 	render() {
 		const {apiKey, sessionId, token, sessionEvents} = this.props;
 		return (
 		<div>
-			<OTSession
-	          apiKey={this.props.apiKey}
-	          sessionId={this.props.sessionId}
-	          token={this.props.token}
-	          eventHandlers={this.sessionEvents}
-	          onError={this.onError}>
-	          {this.props.error ? <div>{this.props.error}</div> : null}
-	          <ConnectionStatus connected={this.props.connected} />
-	          <Publisher />
-	          <OTStreams>
-	            <Subscriber />
-	          </OTStreams>
-	        </OTSession>
+			<Grid>
+				<Grid.Row>
+					<Grid.Column>
+						<Header as='h1'>Remotely Care</Header>
+					</Grid.Column>
+				</Grid.Row>
+				<OTSession
+          apiKey={this.props.apiKey}
+          sessionId={this.props.sessionId}
+          token={this.props.token}
+          eventHandlers={this.sessionEvents}
+          onError={this.onError}>
+          {this.props.error ? <div>{this.props.error}</div> : null}
+          <ConnectionStatus connected={this.props.connected} />
+          <Publisher />
+          <OTStreams>
+	         <Subscriber />
+          </OTStreams>
+        </OTSession>
+	    </Grid>
 		</div>);
 	}
 }
