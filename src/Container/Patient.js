16a7985ea7922027fb5d940d4dc406718384c61a
Zachary Lim <zachary.lim@hotmail.com>
2018-05-06 08:42:51 -0700

    fix connection status

--- src/Container/Patient.js
+++ src/Container/Patient.js
@@ -5,8 +5,26 @@ import Publisher from '../Component/Publisher';
 import Subscriber from '../Component/Subscriber';
 
 class Patient extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+	      error: null,
+	      connected: false,
+	    };
+
+		this.sessionEvents = {
+	      sessionConnected: () => {
+	        this.setState({ connected: true });
+	      },
+	      sessionDisconnected: () => {
+	        this.setState({ connected: false });
+	      }
+	    };
+	}
+
 	render() {
-		const {apiKey, sessionId, token, sessionEvents} = this.props;
+		const { apiKey, sessionId, token } = this.props;
 		return (
 		<div>
 			<OTSession
@@ -16,7 +34,7 @@ class Patient extends Component {
 	          eventHandlers={this.sessionEvents}
 	          onError={this.onError}>
 	          {this.props.error ? <div>{this.props.error}</div> : null}
-	          <ConnectionStatus connected={this.props.connected} />
+	          <ConnectionStatus connected={this.state.connected} />
 	          <Publisher />
 	        </OTSession>
 		</div>);
