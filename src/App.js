4b886a323ca3db072162d2c63e79ed948f20429f
Zachary Lim <zachary.lim@hotmail.com>
2018-05-05 21:15:31 -0700

    add tokbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { OT, OTSession, OTStreams, preloadScript } from 'opentok-react';
+import ConnectionStatus from './Component/ConnectionStatus';
+import Publisher from './Component/Publisher';
+import Subscriber from './Component/Subscriber';
+import config from './config';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      connected: false
+    };
+
+    this.sessionEvents = {
+      sessionConnected: () => {
+        this.setState({ connected: true });
+      },
+      sessionDisconnected: () => {
+        this.setState({ connected: false });
+      }
+    };
+  }
+
+  // componentWillMount() {
+  //   OT.registerScreenSharingExtension('chrome', config.CHROME_EXTENSION_ID, 2);
+  // }
+
+  onError = (err) => {
+    this.setState({ error: `Failed to connect: ${err.message}` });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <OTSession
+        apiKey={this.props.apiKey}
+        sessionId={this.props.sessionId}
+        token={this.props.token}
+        eventHandlers={this.sessionEvents}
+        onError={this.onError}
+      >
+        {this.state.error ? <div>{this.state.error}</div> : null}
+        <ConnectionStatus connected={this.state.connected} />
+        <Publisher />
+        <OTStreams>
+          <Subscriber />
+        </OTStreams>
+      </OTSession>
     );
   }
 }
 
-export default App;
+export default preloadScript(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import config from './config';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App
+    apiKey={config.API_KEY}
+    sessionId={config.SESSION_ID}
+    token={config.TOKEN}
+    loadingDelegate={<div>Loading...</div>}
+    opentokClientUrl="https://static.opentok.com/v2/js/opentok.min.js"
+  />, document.getElementById('root'));
 registerServiceWorker();
